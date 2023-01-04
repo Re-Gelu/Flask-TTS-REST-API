2d907794741082160df6803d34817fa7823cd138
Re:Gelu <morandva@yandex.ru>
2023-01-05 01:04:06 +0300

    Added UI for using API

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,97 +1,44 @@
 jQuery(document).ready( () => {
 
-$.ajaxSetup({
-    "headers": {
-        "X-CSRFToken": csrf_token
-    }
-});
-
 const basic_url =  window.location.protocol + '//' + window.location.host + '/';
 
+/* Upload event handler */
 
-/* Cart buttons event handler */
-const event = (data, textStatus) => {
-    console.log(textStatus, data);
-    $('#dashboard-cart').load(basic_url + 'api/dashboard_cart/');
-    $('#cart-offcanvas-body').load(basic_url + 'api/header_offcanvas_body/');
-};
-
-const cart_event_handler = (ids, data) => {
-
-    document.querySelectorAll(ids).forEach( (element) => {
-        element.onclick = () => {
-            $.post(
-                basic_url + 'api/cart/',
-                $.extend(
-                    {"id": parseInt(element.getAttribute("product-id"))},
-                    data
-                ),
-                event
-            );
-
-            if (element.getAttribute("id") === "cart-add-one-btn") {
-                cart_animation(element);
-            }
-        };
+$('#uploadForm').submit( (event) => {
+    event.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: basic_url + 'api/tts',
+        cache: false,
+        contentType: false,
+        processData: false,
+        data: new FormData(document.querySelector('#uploadForm')),
+        dataType : 'json',
+        success: function(data) {
+            $('#downloadResultLink').html($('<div class="spinner-border" role="status"><span class="visually-hidden">Идёт обработка...</span></div>'))
+            get_task_info(data);
+        }
     });
-
-};
-
-/* Event handler to add one piece of product in cart */
-cart_event_handler(
-    ids='\
-        #cart-add-one-btn, \
-        #cart-dashboard-add-one-btn, \
-        #cart-offcanvas-add-one-btn',
-    data={
-        "action": true,
-        "amount": 1,
-    }
-);
-
-/* Event handler to remove one piece of product from cart */
-cart_event_handler(
-    ids='\
-        #cart-remove-one-btn, \
-        #cart-dashboard-remove-one-btn, \
-        #cart-offcanvas-remove-one-btn',
-    data={
-        "action": false,
-        "amount": 1
-    }
-);
-
-/* Event handler to remove one product position from cart */
-cart_event_handler(
-    ids='\
-        #cart-remove-btn, \
-        #cart-dashboard-remove-btn, \
-        #cart-offcanvas-remove-btn',
-    data={
-        "action": false,
-        "amount": 100
-    }
-);
+});
 
 
-/* Cart animation */
-const cart_animation = (element) => {
-    var img = $(element).closest('#product-card').find('#product-img');
-    var cart = $("#shopping-cart-offcanvas-open-button");
-    img.clone()
-        .removeClass('card-img-top h-100 uk-object-scale-down p-2 uk-transition-opaque')
-        .css({'width' : img.width(),
-            'position' : 'absolute',
-            'z-index' : '9999',
-            top: img.offset().top,
-            left:img.offset().left})
-        .appendTo("body")
-        .animate({opacity: 0.05,
-            left: cart.offset()['left'],
-            top: cart.offset()['top'],
-            width: 20}, 1000, () => {	
-            $(this).remove();
-        });
+/* Get task info every 1 sec  */
+
+const get_task_info = (data) => {
+    const task_url = data.task_url;
+    setTimeout(() => {
+        $.getJSON(
+            url = task_url,
+            success = (data) => {
+                if (data.task_status === 'SUCCESS') {
+                    if (data.task_result_url) {
+                        $('#downloadResultLink').text("Скачать результат!");
+                        $('#downloadResultLink').attr('href', data.task_result_url);
+                    }
+                }
+            }
+        );
+    }, 1000);
 };
 
 });
